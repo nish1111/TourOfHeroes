413918d7ab5c3618cef20e4ba9e181cdd1bc02ed
nish1111 <nishka.maharaj@chillisoft.co.za>
2018-07-04 09:15:59 +0200

    Added more heroes to a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=[
+    {id:11,name:'Superman'},
+    {id:12,name:'Batman'},
+    {id:13,name:'Ironman'},
+    {id:14,name:'Wonder woman'},
+    {id:15,name:'Mr incredible'},
+    {id:16,name:'Cat woman'},
+    {id:17,name:'Spiderman'}
+
+];
